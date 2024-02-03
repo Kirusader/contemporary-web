474a8b7a0238cc15b46639597c6f954de9f748f8
Kirusader <92996204+Kirusader@users.noreply.github.com>
2024-02-03 15:07:48 +0000

    Facebook and Google commit
    Facebook and Google sign in added

--- frontend/src/firebaseconfig.js
+++ frontend/src/firebaseconfig.js
@@ -1,10 +1,27 @@
 /** @format */
+
 // Firebase v9+ uses a different import style
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
+} from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  doc,
+  getDoc,
+} from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
+
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD8NpuEss9sZK-aZzehcrIjiqBaTyPHwC4",
   authDomain: "student-hub-cddbb.firebaseapp.com",
@@ -15,10 +32,84 @@ const firebaseConfig = {
   messagingSenderId: "828903681472",
   appId: "1:828903681472:web:8e4a32ffaf6d4b597d2c25",
 };
+
+// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+// Firebase services
 const auth = getAuth(firebaseApp);
 const provider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 const db = getFirestore(firebaseApp);
 const database = getDatabase(firebaseApp);
 const storage = getStorage(firebaseApp);
-export { storage, database, db, auth, provider };
+// SignIn with FaceBook
+const signInWithFacebook = async (navigate, dispatch) => {
+  try {
+    const res = await signInWithPopup(auth, facebookProvider);
+    const user = res.user;
+    console.log(user);
+    await checkAndAddUser(user);
+    handleLoginSuccess(user, dispatch);
+    navigate("/"); // Using passed navigate function
+  } catch (error) {
+    console.error("Login failed: ", error);
+    if (error.code === "auth/popup-closed-by-user") {
+      console.log("User closed login popup.");
+    }
+  }
+};
+
+// SignIn with Google
+const signInWithGoogle = async (navigate, dispatch) => {
+  try {
+    const res = await signInWithPopup(auth, provider);
+    const user = res.user;
+    await checkAndAddUser(user);
+    handleLoginSuccess(user, dispatch);
+    navigate("/"); // Using passed navigate function
+  } catch (error) {
+    console.error("Login failed: ", error);
+    if (error.code === "auth/popup-closed-by-user") {
+      console.log("User closed login popup.");
+    }
+  }
+};
+
+// Check and add user in Firestore
+const checkAndAddUser = async (user) => {
+  const q = query(collection(db, "users"), where("uid", "==", user.uid));
+  const docs = await getDocs(q);
+  if (docs.docs.length === 0) {
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      username: user.displayName,
+      email: user.email,
+      createdAt: new Date(),
+    });
+    console.log("New user added to Firestore.");
+  }
+};
+
+// Handle login success
+const handleLoginSuccess = async (user, dispatch) => {
+  const token = user.accessToken;
+  const uid = user.uid;
+  const username = user.displayName;
+  localStorage.setItem("token", token);
+  localStorage.setItem("username", username);
+  localStorage.setItem("uid", uid);
+
+  dispatch({ type: "LOGIN_SUCCESS", payload: { uid, token, username } });
+};
+
+// Exports
+export {
+  storage,
+  database,
+  db,
+  auth,
+  provider,
+  signInWithGoogle,
+  signInWithFacebook,
+};
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,8 +6,11 @@ import { Form, Button } from "react-bootstrap";
 import { useAuth } from "../Reducer";
 import { auth } from "../firebaseconfig.js";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { db } from "../firebaseconfig.js";
+import { db, signInWithGoogle, signInWithFacebook } from "../firebaseconfig.js";
 import { doc, getDoc } from "firebase/firestore";
+import { Google } from "../assets/Google.jsx";
+import { Facebook } from "../assets/Facebook.jsx";
+
 function Login() {
   const { dispatch } = useAuth();
   const [email, setEmail] = useState("");
@@ -57,6 +60,14 @@ function Login() {
       );
     }
   };
+  const handleGoogleLogin = async () => {
+    event.preventDefault();
+    await signInWithGoogle(navigate, dispatch);
+  };
+  const handleFacebookLogin = async () => {
+    event.preventDefault();
+    await signInWithFacebook(navigate, dispatch);
+  };
   return (
     <div
       className="container"
@@ -93,6 +104,73 @@ function Login() {
             style={{ margin: "1rem 3rem 0rem 0rem " }}>
             Login
           </Button>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "2px",
+            }}>
+            <hr
+              style={{
+                width: "45%",
+                margin: 0,
+                borderWidth: "2px",
+                borderColor: "white",
+              }}
+            />
+            <p style={{ margin: "0 10px", color: "white" }}>OR</p>
+            <hr
+              style={{
+                width: "45%",
+                margin: 0,
+                borderWidth: "2px",
+                borderColor: "white",
+              }}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-around",
+              padding: "2px",
+            }}>
+            <button
+              onClick={() => handleGoogleLogin()}
+              style={{
+                backgroundColor: "#002ead",
+                color: "white",
+                padding: "15px",
+                border: "none",
+                borderRadius: "4px",
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                marginRight: "2px",
+              }}>
+              <Google style={{ marginRight: "8px" }} />
+              <p style={{ margin: 0 }}>Sign in with Google</p>
+            </button>
+            <button
+              onClick={() => handleFacebookLogin()}
+              style={{
+                backgroundColor: "#4267B2",
+                color: "white",
+                padding: "15px",
+                border: "none",
+                borderRadius: "4px",
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}>
+              <Facebook style={{ marginRight: "8px" }} />
+              <p style={{ margin: 0 }}>Sign in with Facebook</p>{" "}
+            </button>
+          </div>
+
           <p>
             Don't have an account? <Link to="/register">Register</Link>
           </p>
